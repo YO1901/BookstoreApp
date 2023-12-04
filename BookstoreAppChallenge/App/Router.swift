ba41dcea9515597dcff81aba4b3c4adb7ff01a68
Yerlan Omarov <yo@communigate.ru>
2023-12-04 18:59:32 +0600

    initial commit

---
+++ BookstoreAppChallenge/App/Router.swift
@@ -0,0 +1,8 @@
+//
+//  Router.swift
+//  BookstoreAppChallenge
+//
+//  Created by Yerlan Omarov on 04.12.2023.
+//
+
+import UIKit
---
+++ BookstoreAppChallenge/MainScreen/View/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  BookstoreAppChallenge
+//
+//  Created by Yerlan Omarov on 04.12.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
