795925268659a6b358476b58b067a0a30aa039a8
ShirobokovNikita <shirobokov97@yandex.ru>
2023-12-06 13:03:23 +0400

    TabBar and NavigationVC

--- BookstoreAppChallenge/App/SceneDelegate.swift
+++ BookstoreAppChallenge/App/SceneDelegate.swift
@@ -14,8 +14,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = MainViewController()
+//        appRouter.start()
+        
+        window.rootViewController = UINavigationController(rootViewController: AppTabBarController())
         self.window = window
         window.makeKeyAndVisible()
     }
