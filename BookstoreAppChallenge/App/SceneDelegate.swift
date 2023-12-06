61ed806b18f48c697004807a1c352b3785261235
ShirobokovNikita <shirobokov97@yandex.ru>
2023-12-06 17:54:05 +0400

    for merge

--- BookstoreAppChallenge/App/SceneDelegate.swift
+++ BookstoreAppChallenge/App/SceneDelegate.swift
@@ -16,7 +16,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-//        appRouter.start()
         
         window.rootViewController = UINavigationController(rootViewController: AppTabBarController())
         self.window = window
