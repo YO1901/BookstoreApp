b7d87f0c5a2722110b62b00a7d5c7da9c9928ec5
Yerlan <dealwithyerlan@gmail.com>
2023-12-06 19:04:14 +0600

    Revert "Main screen Nikita"

--- BookstoreAppChallenge/MainScreen/View/MainViewController.swift
+++ BookstoreAppChallenge/MainScreen/View/MainViewController.swift
@@ -6,34 +6,14 @@
 //
 
 import UIKit
-import SnapKit
 
 class MainViewController: UIViewController {
-    
-    lazy var button: UIButton = {
-        let button = UIButton()
-        button.setTitle("MainVC", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
-        return button
-    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .white
-        view.addSubview(button)
-        setupLayout()
+        // Do any additional setup after loading the view.
     }
-    
-    func setupLayout() {
-        button.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-        }
-    }
-    
-    @objc func didTapButton() {
-        self.navigationController?.pushViewController(BooksViewController(), animated: true)
-    }
+
+
 }
 
