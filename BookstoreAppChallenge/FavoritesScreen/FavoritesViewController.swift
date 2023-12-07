ec8df4a277ef3fe4011a30957fde9daa31280ebe
Artem Gerasimov <artemtrim@mail.ru>
2023-12-07 13:53:34 +0300

    add UI in FavoritesViewController

--- BookstoreAppChallenge/FavoritesScreen/FavoritesViewController.swift
+++ BookstoreAppChallenge/FavoritesScreen/FavoritesViewController.swift
@@ -8,28 +8,68 @@
 import UIKit
 import SnapKit
 
-class FavoritesViewController: UIViewController {
+final class FavoritesViewController: UIViewController {
     
-    lazy var label: UILabel = {
-        let label = UILabel()
-        label.text = "FavoritesVC"
-        label.textColor = .black
-        label.font = .systemFont(ofSize: 30)
-        return label
+    //MARK: - Properties
+    
+    private let favoritesTableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(FavoritesTableViewCell.self, forCellReuseIdentifier: FavoritesTableViewCell.identifier)
+        return table
     }()
-
+    
+    //MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         view.backgroundColor = .white
-        view.addSubview(label)
+        view.addSubview(favoritesTableView)
         setupLayout()
+        setupTableView()
     }
     
-    func setupLayout() {
-        label.snp.makeConstraints { make in
-            make.center.equalToSuperview()
+    //MARK: - Functions
+    
+    private func setupTableView() {
+        favoritesTableView.delegate = self
+        favoritesTableView.dataSource = self
+        favoritesTableView.rowHeight = 140
+        favoritesTableView.sectionFooterHeight = 0
+        favoritesTableView.backgroundColor = .white
+    }
+    
+    private func setupLayout() {
+        favoritesTableView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(20)
+            make.left.equalToSuperview().offset(20)
+            make.right.equalToSuperview().inset(20)
+            make.bottom.equalToSuperview()
         }
     }
+}
+
+// MARK: - UITableViewDataSource
+
+extension FavoritesViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: FavoritesTableViewCell.identifier, for: indexPath) as? FavoritesTableViewCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension FavoritesViewController: UITableViewDelegate {
+    
 }
