d81369d8e4f9a59bb6f92bbc068a53a1f88033f0
Victor <kavaka08@mail.ru>
2023-12-13 01:19:52 +0300

    add dark-light theme button

--- BookstoreAppChallenge/ViewController.swift
+++ BookstoreAppChallenge/ViewController.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
+    
+    private var cancellables = [AnyCancellable]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,6 +21,13 @@ class ViewController: UIViewController {
             target: nil,
             action: nil
         )
+        
+        overrideUserInterfaceStyle = UserInterfaceStyleService.shared.userInterfaceStyle
+        UserInterfaceStyleService.shared.$userInterfaceStyle.sink {
+            [weak self] style in
+            
+            self?.overrideUserInterfaceStyle = style
+        }.store(in: &cancellables)
 
     }
 }
